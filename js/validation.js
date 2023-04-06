bdf393e28a276f23277ffee8b6dcd5344137e62e
Vladislav Mayantsev <Vladislav678300@mail.ru>
2023-04-07 01:32:29 +0300

    8.19

---
+++ js/validation.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('.img-upload__form');
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__text',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'img-upload__text',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error',
+});
+
+pristine.addValidator(document.querySelector('.text__description'), validateComment, 'Comment length from 20 to 140 characters');
+
+function validateComment(element) {
+  return element.length >= 20 && element.length <= 140;
+}
+
+form.addEventListener('submit', (evt) => {
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
+});
